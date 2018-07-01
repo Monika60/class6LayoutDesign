2920605374a33b91d660e3e320dfce81b47b5b46
Monika60 <monikabiswas60@gmail.com>
2018-07-01 17:13:53 +0600

    Update

--- app/src/main/java/com/example/m/myapplication/MainActivity.java
+++ app/src/main/java/com/example/m/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.m.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSecondActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void openGoogle(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
